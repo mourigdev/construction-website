907170bc05c429d3add57c791b47279d3ff36b07
MOURIG ABDELLAH <mourig.abdellah.dev@gmail.com>
2023-07-23 02:28:58 +0100

    fixing cards

--- src/components/WorkWith.js
+++ src/components/WorkWith.js
@@ -7,7 +7,8 @@ const WorkWith = ({data,title}) => {
       <h3 className="text-green-700 font-bold tracking-wider mb-10">
         {title}
       </h3>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-11/12 mx-auto">
+      <div className={data.length >6 ? `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-11/12 mx-auto` 
+                      : `grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 w-11/12 mx-auto`}>
       
         {data.map((d) => (
           <WorkWithItem
